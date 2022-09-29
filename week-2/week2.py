dbd0aa7489d0db43d761bda6aa2433466ed9f56d
zhizhidory <105404096+zhizhidory@users.noreply.github.com>
2022-09-29 11:41:55 +0800

    Add files via upload

---
+++ week-2/week2.py
@@ -0,0 +1,116 @@
+def calculate(min, max, step):# 請用你的程式補完這個函式的區塊
+    sum=0
+    for n in range(min, max+1, step):
+        sum=sum+n
+    print (sum)
+calculate(1, 3, 1) # 你的程式要能夠計算 1+2+3，最後印出 6
+calculate(4, 8, 2) # 你的程式要能夠計算 4+6+8，最後印出 18
+calculate(-1, 2, 2) # 你的程式要能夠計算 -1+1，最後印出 0
+
+def avg(data):# 請用你的程式補完這個函式的區塊
+    salarylist=[]
+    for listdic in (data["employees"]):
+        if listdic["manager"]==False:
+            salary=listdic["salary"]
+            salarylist+=[salary]
+    sum=0
+    for n in salarylist:
+        sum+=n
+    print (sum/len(salarylist))   
+     
+avg({
+"employees":[
+    {
+    "name":"John",
+    "salary":30000,
+    "manager":False
+    },
+    {
+    "name":"Bob",
+    "salary":60000,
+    "manager":True
+    },
+    {
+    "name":"Jenny",
+    "salary":50000,
+    "manager":False
+    },
+    {
+    "name":"Tony",
+    "salary":40000,
+    "manager":False
+    },
+    ]
+}) # 呼叫 avg 函式
+
+def func(a):# 請用你的程式補完這個函式的區塊
+    def fun2 (b, c):
+        n=b*c
+        result=a+n
+        print (result)
+    return fun2
+func(2)(3, 4) # 你補完的函式能印出 2+(3*4) 的結果 14
+func(5)(1, -5) # 你補完的函式能印出 5+(1*-5) 的結果 0
+func(-3)(2, 9) # 你補完的函式能印出 -3+(2*9) 的結果 15
+# 一般形式為 func(a)(b, c) 要印出 a+(b*c) 的結果
+
+def maxProduct(nums): # 請用你的程式補完這個函式的區塊
+    one=nums[0]
+    two=nums[1]
+    for i in range(1, len(nums)):
+        if nums[i]>one:
+            two=one
+            one=nums[i]
+        elif nums[i]>two:
+            two=nums[i]
+        else :
+            pass
+    large2=two*one
+    for n in range(1, len(nums)):
+        if nums[n]<one:
+            two=one
+            one=nums[n]
+        elif nums[n]<two:
+            two=nums[n]
+    small2=two*one
+    print(max(large2, small2))
+maxProduct([5, 20, 2, 6]) # 得到 120
+maxProduct([10, -20, 0, 3]) # 得到 30
+maxProduct([10, -20, 0, -3]) # 得到 60
+maxProduct([-1, 2]) # 得到 -2
+maxProduct([-1, 0, 2]) # 得到 0
+maxProduct([5,-1, -2, 0]) # 得到 2
+maxProduct([-5, -2]) # 得到 10
+
+
+
+def twoSum(nums, target):# your code here
+    for i in range(0, len(nums)):
+        for n in range(i+1, len(nums)):
+            sum=nums[i]+nums[n]
+            if sum==target:
+                return ([i,n])
+result=twoSum([2, 11, 7, 15], 9)
+print(result) # show [0, 2] because nums[0]+nums[2] is 9
+
+def maxZeros(nums):
+    zero=[]
+    if 0 in nums:
+        for n in range(0,len(nums)):
+            for i in range(n+1, len(nums)):
+                sum=nums[n]+nums[i]
+                if sum!=0:
+                    zero=zero+[i-n]
+                    break
+                else :
+                    zero=zero+[i-n+1]
+        print(max(zero))
+    else:
+        print(0)
+# 請用你的程式補完這個函式的區塊
+maxZeros([0, 1, 0, 0]) # 得到 2
+maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0]) # 得到 4
+maxZeros([1, 1, 1, 1, 1]) # 得到 0
+maxZeros([0, 0, 0, 1, 1]) # 得到 3
+
+
